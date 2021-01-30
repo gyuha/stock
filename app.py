f577c5fe8acc350fbd02cbd06d3c80761e658dab
gyuha <nicegyuha@gmail.com>
2021-01-30 10:37:43 +0900

    기본 라우터 네임 스페이스 추가

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from crawler.crawler import Crawler
 import os
 from flask import Flask
 from flask_restx import Resource, Api
@@ -6,13 +7,23 @@ from dotenv import load_dotenv
 load_dotenv(verbose=True)
 
 app = Flask(__name__)
-api = Api(app)
+api = Api(app,
+          version="0.1",
+          title="Stock - Show me the money",
+          description="주식을 해 볼까 해서 제작 중.",
+          terms_url="/",
+          contact="nicegyuha@gmail.com",
+          license="MIT"
+          )
 
 
 @api.route('/hello')
 class HelloWorld(Resource):
   def get(self):
     return {'hello': os.getenv('DB_NAME')}
+
+
+api.add_namespace(Crawler, '/crawler')
 
 
 if __name__ == '__main__':
---
+++ crawler/crawler.py
@@ -0,0 +1,18 @@
+from flask import request
+from flask_restx import Resource, Api, Namespace, fields
+
+crawler = {}
+
+Crawler = Namespace(
+    name="크롤러",
+    description="자료를 수집한다."
+)
+
+
+@Crawler.route('/companies')
+class Companies(Resource):
+  @Crawler.response(201, 'Success')
+  def get(self):
+    return {
+        'result': True
+    }
